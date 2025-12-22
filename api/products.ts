3738903aa6aea32535a2337121ed9a0d32bb643e
Sirlon Thiago <sirlon.lacerda@ifg.edu.br>
2025-12-22 10:07:45 -0300

    Fix products

--- api/products.ts
+++ api/products.ts
@@ -10,40 +10,91 @@ export default function handler(req: VercelRequest, res: VercelResponse) {
         return res.status(204).end()
     }
 
-    // Lista de nomes fictícios para produtos
-    const productNames = [
-        "Camiseta Premium",
-        "Notebook XFast 15",
-        "Fone de Ouvido Bass+",
-        "Tênis Runner Pro",
-        "Cafeteira Automática",
-        "Smartwatch FitBand",
-        "Mouse Gamer Ultra",
-        "Mochila Adventure",
-        "Cadeira Ergonômica Pro",
-        "Monitor WideVision 27"
-    ]
+    const baseUrl = "https://ppw-1-tads.vercel.app"
 
-    // Função para gerar preços aleatórios entre min e max
-    function randomPrice(min: number, max: number) {
-        return parseFloat((Math.random() * (max - min) + min).toFixed(2))
-    }
-
-    // Lorem ipsum simples
-    const loremIpsum =
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan urna. Donec ultricies nulla at nibh vehicula, a placerat libero dapibus."
-
-    // Gerar lista de produtos
-    const products = productNames.map((name, index) => {
-        const id = index + 1
-        return {
-            id,
-            name,
-            description: loremIpsum,
-            price: randomPrice(50, 500),
-            image: `https://picsum.photos/seed/product${id}/300/200`
+    // Lista de produtos fictícios
+    const products = [
+        {
+            id: 1,
+            name: "Camiseta Premium",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed sit amet accumsan urna.",
+            price: 79.90,
+            image: `${baseUrl}/images/camiseta.png`
+        },
+        {
+            id: 2,
+            name: "Notebook XFast 15",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec ultricies nulla at nibh vehicula.",
+            price: 4599.00,
+            image: `${baseUrl}/images/notebook.jpg`
+        },
+        {
+            id: 3,
+            name: "Fone de Ouvido Bass+",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean euismod bibendum laoreet.",
+            price: 299.90,
+            image: `${baseUrl}/images/fone.png`
+        },
+        {
+            id: 4,
+            name: "Tênis Runner Pro",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin gravida dolor sit amet lacus.",
+            price: 499.90,
+            image: `${baseUrl}/images/tenis.jpg`
+        },
+        {
+            id: 5,
+            name: "Cafeteira Automática",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam erat volutpat.",
+            price: 899.00,
+            image: `${baseUrl}/images/cafeteira.jpg`
+        },
+        {
+            id: 6,
+            name: "Smartwatch FitBand",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur non nulla sit amet.",
+            price: 649.90,
+            image: `${baseUrl}/images/smartwatch.jpg`
+        },
+        {
+            id: 7,
+            name: "Mouse Gamer Ultra",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla porttitor accumsan tincidunt.",
+            price: 189.90,
+            image: `${baseUrl}/images/mouse.jpg`
+        },
+        {
+            id: 8,
+            name: "Mochila Adventure",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum ante ipsum primis.",
+            price: 229.90,
+            image: `${baseUrl}/images/mochila.jpg`
+        },
+        {
+            id: 9,
+            name: "Cadeira Ergonômica Pro",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt.",
+            price: 1299.00,
+            image: `${baseUrl}/images/cadeira.jpg`
+        },
+        {
+            id: 10,
+            name: "Monitor WideVision 27",
+            description:
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis aute irure dolor in reprehenderit.",
+            price: 1799.00,
+            image: `${baseUrl}/images/monitor.jpg`
         }
-    })
+    ]
 
     return res.status(200).json({
         success: true,
